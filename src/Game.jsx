7f159a4d43f270a10652413617fb72c0c0cddaee
n0eL <dev@n0el1405.live>
2026-04-30 23:53:33 +0200

    - game card styling (wip)

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,21 +1,42 @@
 import './Game.css'
 
-export default function Game({ data }) {
+function openInNewTab(url) {
+  window.open(url, '_blank', 'noopener,noreferrer').focus();
+}
 
-  return(
-      <div className="game-card card">
-        <img src={data.imgUrl} alt={data.name}
-             className="game-card-img card-img-top"
-             loading="lazy"/>
-        <div className="card-body">
-          <h3 className={"card-title fw-bold"}>{data.name}</h3>
-          <p className={"card-subtitle"}>{data.id}</p>
-          <div className="card-text">
-            <p>Price: {data.price}</p>
-            <p>Number of Cards: {data.numberOfCards}</p>
-            <p>Score: {data.score.toFixed(2)}</p>
+export default function Game({data}) {
+
+  return (
+
+      <div className={"row game-card"}>
+        <div className={"col col-3 img-col"}>
+          <img src={data.imgUrl} alt={data.name} loading="lazy"/>
+        </div>
+        <div className={"col game-info-col"}>
+          <div className={"row row-2"}>
+            <h3 className={"fw-bold"}>{data.name}</h3>
           </div>
-          <div className="actions-row card-footer"></div>
+          <div className={"row"}>
+            <p>💰 {data.price}</p>
+            <p>🎴 {data.numberOfCards}</p>
+            <p>💯 {data.score.toFixed(2)}</p>
+          </div>
+          <div className={"row row-1 actions-row"}>
+            <div className={"col"}>
+              <button className="btn btn-primary" onClick={() => openInNewTab(
+                  `https://store.steampowered.com/app/${data.id}`)}>Store
+              </button>
+            </div>
+            <div className={"col"}>
+              <button className="btn btn-secondary" onClick={() => openInNewTab(
+                  `https://steamdb.info/app/${data.id}/`)}>SteamDB
+              </button>
+            </div>
+            <div className={"col"}>
+              <button className="btn btn-danger" disabled={true}>Ignore</button>
+            </div>
+
+          </div>
         </div>
       </div>
   )
